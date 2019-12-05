3db7d023a8351cc8abcd631b7e1d19531cfed566
Ильдар Нигметзянов <>
2019-12-05 23:17:14 +0300

    Added functions

---
+++ BirthdayNotes/BirthdayNotes/Controllers/InputAlertController.swift
@@ -0,0 +1,30 @@
+//
+//  InputAlertController.swift
+//  BirthdayNotes
+//
+//  Created by Ильдар Нигметзянов on 04.12.2019.
+//  Copyright © 2019 SberBank. All rights reserved.
+//
+
+import UIKit
+
+class InputAlertController: UIAlertController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
